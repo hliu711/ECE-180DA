60939bc717a27542bfd2c7ad1c552ef5a025a10a
hliu711 <55860435+hliu711@users.noreply.github.com>
2019-09-27 18:29:59 -0700

    Reference: https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/p…

--- camera.py
+++ camera.py
@@ -1,6 +1,6 @@
 #Reference: 
 #https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
-
+#Changing captured video from gray to HSV for better color segmentation
 
 import numpy as np
 import cv2
@@ -12,13 +12,12 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # Display the resulting frame
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
